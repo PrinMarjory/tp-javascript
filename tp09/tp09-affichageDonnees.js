8026e3836f3045375f54a7a3b89651bcac9b2a7c
Marjory PRIN <marjory.prin@gmail.com>
2024-06-20 13:37:48 +0200

    TP09 - Exo 2

--- tp09/tp09-affichageDonnees.js
+++ tp09/tp09-affichageDonnees.js
@@ -1,13 +1,24 @@
+//Récupérer les données avec AJAX
 function init() {
     // Création de la requête AJAX
     let req = new XMLHttpRequest();
     req.open("GET", 'https://restcountries.com/v3.1/all');
     req.onreadystatechange = function () {
         if (req.readyState == 4 && req.status == 200) {
-            let data = req.responseText;
+            let data = JSON.parse(req.responseText); 
             let zoneAffichage = document.querySelector("#contenu");
-            zoneAffichage.innerHTML = data;
-        }
-    };
+            for (let i=0 ; i<data.length ; i++){ 
+                let nomPays = data[i].name.common;
+                let capitalePays = data[i].capital;
+                let populationPays = data[i].population;
+                let regionPays = data[i].region;  
+                
+                //Création des lignes par pays
+                let ligne = document.createElement('p')
+                ligne.textContent = `${nomPays}, capitale: ${capitalePays}, population: ${populationPays}, région: ${regionPays}`;
+                zoneAffichage.appendChild(ligne);
+            }
+        }  
+    }
     req.send();
 }
